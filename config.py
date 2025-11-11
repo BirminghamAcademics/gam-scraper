20308494d03c3f9888f3bb8c14f9699fb98d4a93
BirminghamAcademics <b.bentley30@OUTLOOK.COM>
2025-11-11 16:39:44 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,2 @@
+DEFAULT_SEARCH_URL = "https://github.com/search?q=open+source+game&type=repositories"
+DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
